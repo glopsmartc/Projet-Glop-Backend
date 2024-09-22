4cfde0082386ce2a41075466eb0033e18f755558
rawanghosn <rawanghosn22@gmail.com>
2024-09-21 17:45:34 -0700

    Initialisation des projets backend et frontend avec la structure de base

---
+++ backend/projetglop-backend/src/main/java/com/projetglop/projetglop/ProjetglopApplication.java
@@ -0,0 +1,13 @@
+package com.projetglop.projetglop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetglopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetglopApplication.class, args);
+	}
+
+}
---
+++ backend/projetglop-backend/src/test/java/com/projetglop/projetglop/ProjetglopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetglop.projetglop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetglopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
