c955cfa6a8332873732cdbcec43b499120f1e8a3
rawanghosn <rawanghosn22@gmail.com>
2024-09-26 23:58:53 -0700

    Changed java version and solved docker and database issues

--- backend/projetglop-backend/src/test/java/com/projetglop_backend/projetglop_backend/ProjetglopBackendApplicationTests.java
+++ backend/projetglop-backend/src/test/java/com/projetglop_backend/projetglop_backend/ProjetglopBackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.projetglop.projetglop;
+package com.projetglop_backend.projetglop_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProjetglopApplicationTests {
+class ProjetglopBackendApplicationTests {
 
 	@Test
 	void contextLoads() {
