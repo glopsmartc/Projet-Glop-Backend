3862f39a9d615cb06516971acdcec16fb7cfc21c
lib2211 <rawanghosn22@gmail.com>
2024-10-11 18:49:47 -0700

    change in GestionDesUtilisateursService/UserRepository

--- GestionDesUtilisateursService/src/main/java/com/gestionUtilisateurs/gestionUtilisateurs/UserRepository.java
+++ GestionDesUtilisateursService/src/main/java/com/gestionUtilisateurs/gestionUtilisateurs/UserRepository.java
@@ -1,6 +1,8 @@
 package com.gestionUtilisateurs.gestionUtilisateurs;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<Utilisateur, Long> {
 }
