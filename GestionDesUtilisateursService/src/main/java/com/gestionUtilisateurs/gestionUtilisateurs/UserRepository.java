a4c9a8c4e6f9106d5f74676b76b6c5da420fd1f6
lib2211 <rawanghosn22@gmail.com>
2024-10-13 11:58:41 -0700

    deleting test classes

--- GestionDesUtilisateursService/src/main/java/com/gestionUtilisateurs/gestionUtilisateurs/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.gestionUtilisateurs.gestionUtilisateurs;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<Utilisateur, Long> {
-}
