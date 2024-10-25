81d7f2f5748d6e2c732bb2f3eb8b5a51c18a7e11
BouchraHayani <bouchra.hayani.etu@univ-lille.fr>
2024-10-25 19:50:09 +0200

    GestionDesUtilisateursServiceApplicationTests

--- GestionDesUtilisateursService/src/test/java/com/gestionUtilisateurs/gestionUtilisateurs/GestionDesUtilisateursServiceApplicationTests.java
+++ GestionDesUtilisateursService/src/test/java/com/gestionUtilisateurs/gestionUtilisateurs/GestionDesUtilisateursServiceApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-class ProjetglopBackendApplicationTests {
+class GestionDesUtilisateursServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
