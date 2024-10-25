8165ced63ba4c472de47188117a2600d13fd738c
BouchraHayani <bouchra.hayani.etu@univ-lille.fr>
2024-10-25 19:47:15 +0200

    setters added

--- GestionDesUtilisateursService/src/main/java/com/gestionUtilisateurs/gestionUtilisateurs/dto/LoginUserDto.java
+++ GestionDesUtilisateursService/src/main/java/com/gestionUtilisateurs/gestionUtilisateurs/dto/LoginUserDto.java
@@ -2,8 +2,10 @@ package com.gestionUtilisateurs.gestionUtilisateurs.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 @AllArgsConstructor
 public class LoginUserDto {
     private String email;
