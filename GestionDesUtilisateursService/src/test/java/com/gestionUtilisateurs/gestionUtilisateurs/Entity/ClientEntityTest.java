e512a8575b798e09ddc32d959c3da43c04a58935
BouchraHayani <bouchra.hayani.etu@univ-lille.fr>
2024-10-25 19:59:18 +0200

    ClientEntityTest

---
+++ GestionDesUtilisateursService/src/test/java/com/gestionUtilisateurs/gestionUtilisateurs/Entity/ClientEntityTest.java
@@ -0,0 +1,44 @@
+package com.gestionUtilisateurs.gestionUtilisateurs.Entity;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.gestionUtilisateurs.gestionUtilisateurs.model.Utilisateur;
+import com.gestionUtilisateurs.gestionUtilisateurs.model.UtilisateurRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.transaction.annotation.Transactional;
+import java.time.LocalDate;
+
+@DataJpaTest
+@Transactional
+public class ClientEntityTest {
+
+    @Autowired
+    private UtilisateurRepository utilisateurRepository;
+
+    private Utilisateur utilisateur;
+
+    @BeforeEach
+    public void setUp() {
+        utilisateur = new Utilisateur();
+        utilisateur.setNom("John");
+        utilisateur.setPrenom("Doe");
+        utilisateur.setEmail("john.doe@example.com");
+        utilisateur.setMotDePasse("strongpassword");
+        utilisateur.setAdresse("123 Rue Exemple");
+        utilisateur.setDateNaissance(LocalDate.of(1990, 1, 1));
+        utilisateur.setNumTel("0123456789");
+    }
+
+    @Test
+    public void testSaveValidClient() {
+        Utilisateur savedUtilisateur = utilisateurRepository.save(utilisateur);
+        assertNotNull(savedUtilisateur.getIdUser());
+        assertEquals(utilisateur.getNom(), savedUtilisateur.getNom());
+        assertEquals(utilisateur.getPrenom(), savedUtilisateur.getPrenom());
+        assertEquals(utilisateur.getEmail(), savedUtilisateur.getEmail());
+    }
+
+}
