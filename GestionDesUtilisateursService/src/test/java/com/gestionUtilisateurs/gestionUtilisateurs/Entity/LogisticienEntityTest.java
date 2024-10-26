a265dc30479e06405e65dd56ff6248b56d878311
lib2211 <rawanghosn22@gmail.com>
2024-10-26 11:30:19 -0700

    GestionDesUtilisateursService/ solving logisticien  tests issues

--- GestionDesUtilisateursService/src/test/java/com/gestionUtilisateurs/gestionUtilisateurs/Entity/LogisticienEntityTest.java
+++ GestionDesUtilisateursService/src/test/java/com/gestionUtilisateurs/gestionUtilisateurs/Entity/LogisticienEntityTest.java
@@ -20,8 +20,8 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
-//@ActiveProfiles("test")
-//@Sql(scripts = "/insert_roles.sql")
+@ActiveProfiles("test")
+@Sql(scripts = "/insert_roles.sql")
 class LogisticienEntityTest {
 
     @Autowired
