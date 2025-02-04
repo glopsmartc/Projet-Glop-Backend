cc1d758b2fd9cc7356b1e39798261d732a480844
lib2211 <rawanghosn22@gmail.com>
2025-02-04 13:38:52 +0100

    resolving test issue in pipeline

--- GestionDesUtilisateursService/src/test/java/com/gestionUtilisateurs/gestionUtilisateurs/service/MailConfigTest.java
+++ GestionDesUtilisateursService/src/test/java/com/gestionUtilisateurs/gestionUtilisateurs/service/MailConfigTest.java
@@ -1,16 +1,15 @@
 package com.gestionUtilisateurs.gestionUtilisateurs.service;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mail.javamail.JavaMailSender;
-
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class MailConfigTest {
 
-    @Autowired
+    @MockBean
     private JavaMailSender javaMailSender;
 
     @Test
