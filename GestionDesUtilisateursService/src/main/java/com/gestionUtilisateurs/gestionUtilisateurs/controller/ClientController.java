967ed12d53fb9346bb894b22d8f21820303a5ddc
lib2211 <rawanghosn22@gmail.com>
2025-02-15 22:43:39 +0100

    MAJ des controllers GestionDesUtilisateursService/ et GestionDesContratsService/

--- GestionDesUtilisateursService/src/main/java/com/gestionUtilisateurs/gestionUtilisateurs/controller/ClientController.java
+++ GestionDesUtilisateursService/src/main/java/com/gestionUtilisateurs/gestionUtilisateurs/controller/ClientController.java
@@ -5,10 +5,7 @@ import com.gestionUtilisateurs.gestionUtilisateurs.service.UserServiceItf;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/clients")
@@ -24,10 +21,10 @@ public class ClientController
     @PostMapping("/updateInfosMaladies")
     @PreAuthorize("hasRole('CLIENT')")
     public ResponseEntity<String> setMaladies(
-            @RequestParam MaladieChroniqueDTO maladieChroniqueDTO) {
+            @RequestBody MaladieChroniqueDTO maladieChroniqueDTO) {
         try {
             userService.updateMaladieChronique(maladieChroniqueDTO);
-            return ResponseEntity.ok("Date de naissance mise à jour avec succès.");
+            return ResponseEntity.ok("Maladies chroniques mise à jour avec succès.");
         } catch (Exception e) {
             return ResponseEntity.status(500).body("Erreur lors de la mise à jour des maladies chroniques : " + e.getMessage());
         }
