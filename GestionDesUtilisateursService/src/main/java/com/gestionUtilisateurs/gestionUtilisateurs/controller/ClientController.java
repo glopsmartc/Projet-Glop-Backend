70d6511ba829376a0193e033b1eb69f12bb61758
lib2211 <rawanghosn22@gmail.com>
2025-02-24 16:31:54 +0100

    liste des clients pour conseiller GestionDesUtilisateursService/

--- GestionDesUtilisateursService/src/main/java/com/gestionUtilisateurs/gestionUtilisateurs/controller/ClientController.java
+++ GestionDesUtilisateursService/src/main/java/com/gestionUtilisateurs/gestionUtilisateurs/controller/ClientController.java
@@ -1,21 +1,25 @@
 package com.gestionUtilisateurs.gestionUtilisateurs.controller;
 
 import com.gestionUtilisateurs.gestionUtilisateurs.dto.MaladieChroniqueDTO;
+import com.gestionUtilisateurs.gestionUtilisateurs.model.Client;
+import com.gestionUtilisateurs.gestionUtilisateurs.service.ClientServiceItf;
 import com.gestionUtilisateurs.gestionUtilisateurs.service.UserServiceItf;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/clients")
 public class ClientController
 {
     private final UserServiceItf userService;
+    private final ClientServiceItf clientServiceItf;
 
-
-    public ClientController(UserServiceItf userService) {
+    public ClientController(UserServiceItf userService, ClientServiceItf clientServiceItf) {
         this.userService = userService;
+        this.clientServiceItf = clientServiceItf;
     }
 
     @PostMapping("/updateInfosMaladies")
@@ -28,5 +32,13 @@ public class ClientController
         } catch (Exception e) {
             return ResponseEntity.status(500).body("Erreur lors de la mise à jour des maladies chroniques : " + e.getMessage());
         }
+    }
+
+    @GetMapping("/")
+    @PreAuthorize("hasRole('CONSEILLER')")
+    public ResponseEntity<List<Client>> allClients() {
+        List <Client> clients = clientServiceItf.allClients();
+
+        return ResponseEntity.ok(clients);
     }
 }
---
+++ GestionDesUtilisateursService/src/main/java/com/gestionUtilisateurs/gestionUtilisateurs/service/ClientServiceItf.java
@@ -0,0 +1,9 @@
+package com.gestionUtilisateurs.gestionUtilisateurs.service;
+
+import com.gestionUtilisateurs.gestionUtilisateurs.model.Client;
+
+import java.util.List;
+
+public interface ClientServiceItf {
+    List<Client> allClients();
+}
